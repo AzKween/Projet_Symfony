01580410d376fc8a14f766e8cf4a7d923a2b3902
D'hornes Rémi <dhornes@hotmail.fr>
2020-12-14 16:21:03 +0100

    initial commit

--- src/Controller/FeatureController.php
+++ src/Controller/FeatureController.php
@@ -6,15 +6,15 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class TutoController extends AbstractController
+class FeatureController extends AbstractController
 {
     /**
-     * @Route("/tuto", name="tuto")
+     * @Route("/feature", name="feature")
      */
     public function index()
     {
-        return $this->render('tuto/index.html.twig', [
-            'controller_name' => 'TutoController',
+        return $this->render('feature/index.html.twig', [
+            'controller_name' => 'FeatureController',
         ]);
     }
 }
